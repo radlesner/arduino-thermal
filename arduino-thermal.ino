be17291bd71df98a4f3aa9fd5366e9efe8613f94
Radosław Lesner <radek3301@linux.pl>
2023-06-01 17:24:37 +0200

    Code fixes and comments

--- arduino-thermal.ino
+++ arduino-thermal.ino
@@ -1,12 +1,13 @@
 /*
-	Used pins for Arduino Nano and Uno:
+	Used pins for Arduino Nano:
+		D9 = Temperature sensor pin
 		D8 = LCD RS
-		D9 = LCD E
+		D7 = LCD RW (Currently not user)
+		D6 = LCD E
 		D5 = LCD Data (D4)
 		D4 = LCD Data (D5)
 		D3 = LCD Data (D6)
 		D2 = LCD Data (D7)
-		D7 = Temperature sensor pin
 */
 
 #include <LiquidCrystal.h>
@@ -15,19 +16,20 @@
 
 #define VERSION "v0.2"
 
-const int rs = 8,
-					en = 6,
-					d4 = 5,
-					d5 = 4,
-					d6 = 3,
-					d7 = 2,
-					tempSensorPin = 9;
+const int
+	rs = 8,
+	en = 6,
+	d4 = 5,
+	d5 = 4,
+	d6 = 3,
+	d7 = 2,
+	tempSensorPin = 9;
 
 int sensorCount = 0;
 
 float tempC;
 
-OneWire oneWire(tempSensorPin);	
+OneWire oneWire(tempSensorPin);
 DallasTemperature sensors(&oneWire);
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
@@ -50,25 +52,29 @@ void degreeSerialSymbol() {
 	}
 }
 
-// ------------------------------------- MAIN -------------------------------------
+// ----------------------------------------------------------------- MAIN -----------------------------------------------------------------
 
 void setup()
 {
+	// ----- Serial settings -----
   sensors.begin();
   Serial.begin(9600);
-  
-  Serial.println("Scanning...");
-  Serial.print("Found ");
+
+	// Scan DSB18B20 sensors
+  Serial.println("> Scanning...");
   sensorCount = sensors.getDeviceCount();
+	Serial.print("> ");
   Serial.print(sensorCount, DEC);
+	Serial.print("found ");
   Serial.println(" devices.");
   Serial.println("");
 
-
+	// ----- LCD settings -----
 	lcd.begin(16, 2);
 	lcd.createChar(0, degreeChar);
-	lcd.setCursor(0, 0);
 
+	// Initial screen output
+	lcd.setCursor(0, 0);
 	lcd.print("Arduino Thermal");
 	lcd.setCursor(0, 1);
 	lcd.print(VERSION);
@@ -77,20 +83,20 @@ void setup()
 
 	lcd.setCursor(0, 0);
 	lcd.print("T1: ---");
-
 	lcd.setCursor(0, 1);
 	lcd.print("T2: ---");
 	delay(500);
 }
 
 void loop()
-{ 
-  sensors.requestTemperatures(); 
-  
+{
+  sensors.requestTemperatures();
+
   for (int i = 0;  i < sensorCount;  i++)
   {
+		// Serial temperature output
     Serial.print("Sensor ");
-    Serial.print(i+1);
+    Serial.print(i + 1);
     Serial.print(": ");
     tempC = sensors.getTempCByIndex(i);
     Serial.print(tempC);
@@ -98,12 +104,13 @@ void loop()
     degreeSerialSymbol();
     Serial.println("C");
 
+		// LCD temperature output
 		lcd.setCursor(4, i);
 		lcd.print(tempC);
 		lcd.write(byte(0));
 		lcd.print("C");
   }
-  
+
   Serial.println("");
 	delay(500);
 }
